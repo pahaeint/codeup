8cc74d990908c0b2747fb949f9bc71d14ed9a2fc
pahaeint <pahae.int@gmail.com>
2024-12-11 22:46:32 +0900

    add 1065

---
+++ C/1065.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n,m,l;
+    scanf("%d %d %d",&n,&m,&l);
+    if(n%2==0){
+        printf("%d\n",n);
+    }
+    if(m%2==0){
+        printf("%d\n",m);
+    }
+    if(l%2==0){
+        printf("%d\n",l);
+    }
+    return 0;
+}
