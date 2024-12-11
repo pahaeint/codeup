41a4a61b097e6762c7d837275265b933463d8784
pahaeint <pahae.int@gmail.com>
2024-12-11 22:47:19 +0900

    add 1066

---
+++ C/1066.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n,m,l;
+    scanf("%d %d %d",&n,&m,&l);
+    if(n%2==0){
+        printf("even\n");
+    }
+    else{
+        printf("odd\n");
+    }
+    if(m%2==0){
+        printf("even\n");
+    }
+    else{
+        printf("odd\n");
+    }
+    if(l%2==0){
+        printf("even\n");
+    }
+    else{
+        printf("odd\n");
+    }
+    return 0;
+}
